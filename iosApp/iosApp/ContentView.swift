71f4e40eb3c91c98ed03f06742fc21498cd24ef7
shadmanadman <adman.shadman@gmail.com>
2025-06-13 13:24:59 +0330

    Refactor

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,8 +4,7 @@ import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
-    }
+   }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
--- kflite/src/iosMain/kotlin/org/kmp/playground/kflite/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package org.kmp.playground.kflite
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { }
